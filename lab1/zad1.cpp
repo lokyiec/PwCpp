8861e5096fdd9c6512c6c01366778b808b8d6667
lokyiec <paulkolano@mail.com>
2020-10-13 19:50:19 +0200

    create dictionary

---
+++ lab1/zad1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    double a = 5.5;
+    double b = 1.5;
+    double sum;
+    sum = a + b;
+    cout << sum << endl;
+    sum = a - b;
+    cout << sum << endl;
+    sum = a * b;
+    cout << sum << endl;
+    sum = a / b;
+    cout << sum << endl;
+
+// 3
+    // int a,b,c,d,e;
+    // double sum;
+    // cout<<"Podaj a: "<<endl;
+    // cin>>a;
+    // cout<<"Podaj b: "<<endl;
+    // cin>>b;
+    // cout<<"Podaj c: "<<endl;
+    // cin>>c;
+    // cout<<"Podaj d: "<<endl;
+    // cin>>d;
+    // cout<<"Podaj e: "<<endl;
+    // cin>>e;
+    // sum=(double)a+b+c+d+e;
+    // cout<<"Suma twoich liczb to: ";
+    // cout<<sum<<endl;
+// 4
+    // double a,b,c,x1,x2,delta,pierwiastek;
+    // cout<<"Podaj a funkcji kwadratowej:"<<endl;
+    // cin>>a;
+    // cout<<"Podaj b funkcji kwadratowej:"<<endl;
+    // cin>>b;
+    // cout<<"Podaj c funkcji kwadratowej:"<<endl;
+    // cin>>c;
+    // delta=b*b-4*a*c;
+    // pierwiastek= sqrt(delta);
+    // x1=(-b+pierwiastek)/2*a;
+    // x2=(-b-pierwiastek)/2*a;
+    // cout<<"Pierwiastki równania to: x1:";
+    // cout<<x1<<endl;
+    // cout<<"x2: ";
+    // cout<<x2<<endl;
+}
---
+++ lab2/lab2.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using  namespace std;
+int main(int argc, char** argv) {
+/* 
+2
+
+	for(int i=0; i<=10;i++){
+		cout<<i<<endl;
+	}
+	int j=0;
+	cout<<endl;
+	while( j<=10){
+		cout<<j<<endl;
+		j++;
+	}
+	cout<<endl;
+	int k=0;
+	do{
+		cout<<k<<endl;
+		k++;
+	}
+	while(k<=10);
+	cout<<endl;
+	
+	for(int ii=10; ii>=0;ii--){
+	cout<<ii<<endl;
+	}
+	
+	int jj=10;
+	cout<<endl;
+	while( jj>=0){
+		cout<<jj<<endl;
+		jj--;
+	}
+	cout<<endl;
+	int kk=10;
+	do{
+		cout<<kk<<endl;
+		kk--;
+	}
+	while(kk>=0);
+
+3
+
+int tab[11][11];
+    for (int i=1; i<11; i++)
+    {
+        for (int j=1; j<11; j++)
+        {
+           
+           
+            tab[i][j]=i*j;
+            
+        }
+        cout<<endl;
+ 
+    }
+ 	cout<<"Tabliczka mnozenia"<<endl<<endl;
+ 	for (int i=1; i<11; i++)
+    {
+        for (int j=1; j<11; j++)
+        {
+           
+           
+            cout<<tab[i][j]<<"\t";
+            
+        }
+        cout<<endl;
+ 
+    }
+    
+4	
+
+int fibo[10];
+fibo[0]=0;
+fibo[1]=1;
+for(int i=2;i<10;i++)
+{
+	fibo[i]=fibo[i-1]+fibo[i-2];
+}
+for (int j=0;j<10;j++){
+	cout<<fibo[j]<<endl;
+}
+
+5
+
+
+int menu;
+cout<<"1.Dodawanie"<<endl<<"2.Odejmowanie"<<endl<<"3.Mnozenie"<<endl<<"4.Dzielenie"<<endl;
+cin>>menu;
+
+
+switch(menu){
+	double a,b,wynik;
+	case 1:
+		cout<<"Dodawanie"<<endl<<"Podaj A:"<<endl;
+		cin>>a;
+		cout<<"Podaj B:"<<endl;
+		cin>>b;
+		wynik=a+b;
+		cout<<"A+B="<<wynik;
+		break;		
+	case 2:
+		cout<<"Odejmowanie"<<endl<<"Podaj A:"<<endl;
+		cin>>a;
+		cout<<"Podaj B:"<<endl;
+		cin>>b;
+		wynik=a-b;
+		cout<<"A-B="<<wynik;
+		break;
+			
+	case 3:
+		cout<<"Mnozenie"<<endl<<"Podaj A:"<<endl;
+		cin>>a;
+		cout<<"Podaj B:"<<endl;
+		cin>>b;
+		wynik=a*b;
+		cout<<"A*B="<<wynik;
+		break;
+			
+	case 4:
+		cout<<"Dzielenie"<<endl<<"Podaj A:"<<endl;
+		cin>>a;
+		cout<<"Podaj B:"<<endl;
+		cin>>b;
+		wynik=a/b;
+		cout<<"A/B="<<wynik;
+		break;
+}
+*/
+ 
+}
+
